053bc76706c53280b7190dd5eccbe49a162d4438
oguzhanl <111123587+oguzhanl@users.noreply.github.com>
2022-10-19 00:45:59 +0300

    Add files via upload

---
+++ website/translate.js
@@ -0,0 +1,11 @@
+function translatePage(){
+  document.querySelector('.my-name').textContent = 'Oğuzhan Ömer';
+  document.querySelector('.department').textContent = 'Yapay Zeka Mühendisliği';
+  document.querySelector('.about-me').textContent = 'Hakkkımda';
+  document.querySelector('.info').textContent = 'Merhaba. Ben Oğuzhan. Bahçeşehir Üniversitesinde yapay zeka mühendisliği okuyorum. Şu anda Html/Css biliyorum ve kendimi zamanla geliştirmeyi düşünüyorum. Sonraki güncellemede görüşürüz.';
+  document.querySelector('.nam-nam').textContent = 'İsim';
+  document.querySelector('.age-age').textContent = 'Yaş';
+  document.querySelector('.dep-dep').textContent = 'Bölüm';
+  document.querySelector('name-again').textContent = 'Oğuzhan Ömer';
+  document.querySelector('department-again').textContent = 'Yapay Zeka Mühendisliği';
+}
